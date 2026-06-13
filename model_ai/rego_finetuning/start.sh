f488c0d4a8110c1cab2d8efe46fef5dbcb898b55
marvior <waltermolino.2025@gmail.com>
2026-06-13 17:04:32 +0200

    readme and bug fix

--- model_ai/rego_finetuning/start.sh
+++ model_ai/rego_finetuning/start.sh
@@ -1,7 +1,7 @@
 python -m mlx_lm.lora \
     --model Qwen/Qwen2.5-Coder-1.5B-Instruct  \
     --data ./data \
-    --adapter-path /Users/waltermolino/rustProjects/mcp-proxy/model_ai/rego_finetuning/adapters \
+    --adapter-path adapters \
     --iters 400 \
     --steps-per-eval 40  \
     --batch-size 2 \
